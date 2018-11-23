6c20f39fb4e6a4621af40dd0e63c3d4912d7d9ae
Peggy Newman <peggydnewman@gmail.com>
2018-11-23 15:31:43 +1100

    have to pull up somewhere!

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # UntweetdAus
 Tweeting the untweetable threatened species
 
-Original idea via [Tweeting for their lives: Visibility of threatened species on twitter](https://www.sciencedirect.com/science/article/pii/S1617138118301183?dgcid=coauthor)
+This is a project from RopenSci ozunconference 2018. See issues for the problems we encountered.
+
+Idea was originally discussed here: https://github.com/ropensci/ozunconf18/issues/13
+and comes about via this paper: [Tweeting for their lives: Visibility of threatened species on twitter](https://www.sciencedirect.com/science/article/pii/S1617138118301183?dgcid=coauthor)
 
--- scripts/tweetTheUntweeted.R
+++ scripts/tweetTheUntweeted.R
@@ -1,15 +1,27 @@
+library(dplyr)
+library(stringr)
+
 specieslist = as.data.frame(0)
 speciestweets = as.data.frame(0)
 mytweets = as.data.frame(0)
 staremoji <- emojis %>% filter(grepl("white medium star",description))
-
 specieslist <- read.csv("data/SpeciesList.csv")
+
+# find the tweets we've already done
 mytweets <- get_my_timeline()
 
-library(dplyr)
+# trying to find the status ids of the tweets we've done before by looking at the url we put in the tweets
+# doh! they've been shortened 
+url_pattern <- "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+for(i in 1:nrow(mytweets)) {
+    url <- str_extract(as.character(mytweets$text[i]),url_pattern)
+    print(url)
+}
 
-for (i in 1:10){ #nrow(speciesList)) {
-  
+# loop through the scientific names in the file
+# search twitter for it
+# if we find it and we didn't post it, tweet it
+for (i in 1:nrow(specieslist)) {
   speciesname = as.character(specieslist$Genus_species[i])
   print(paste("i=",i,"|",speciesname))
   speciestweets = search_tweets(speciesname)
@@ -18,9 +30,10 @@ for (i in 1:10){ #nrow(speciesList)) {
     for (j in 1:nrow(speciestweets)) {
       # check if this is one of our tweets
       thisscreenname = as.character(speciestweets$screen_name[j])
+      thisstatusid = as.character(speciestweets$status_id[j])
       print(thisscreenname)
       if (thisscreenname != my_screen_name) {
-        post_tweet(status = paste(staremoji$code," Another #untweetableSpecies! ", speciestweets$status_url[j]))
+        post_tweet(status = paste(staremoji$code," An #untweetableSpecies was tweeted! ", speciestweets$status_url[j]))
       } else {
         print("this was my tweet")
       }
