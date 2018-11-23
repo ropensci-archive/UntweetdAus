1cef745056c0719e63cf2a9f8d611d27dca78fd2
Peggy Newman <peggydnewman@gmail.com>
2018-11-23 14:34:44 +1100

    added a gold star

--- scripts/tweetTheUntweeted.R
+++ scripts/tweetTheUntweeted.R
@@ -1,6 +1,7 @@
 specieslist = as.data.frame(0)
 speciestweets = as.data.frame(0)
 mytweets = as.data.frame(0)
+staremoji <- emojis %>% filter(grepl("white medium star",description))
 
 specieslist <- read.csv("data/SpeciesList.csv")
 mytweets <- get_my_timeline()
@@ -19,23 +20,13 @@ for (i in 1:10){ #nrow(speciesList)) {
       thisscreenname = as.character(speciestweets$screen_name[j])
       print(thisscreenname)
       if (thisscreenname != my_screen_name) {
-        post_tweet(status = paste("Another #untweetableSpecies ", speciestweets$status_url[j]))
+        post_tweet(status = paste(staremoji$code," Another #untweetableSpecies! ", speciestweets$status_url[j]))
       } else {
-        print("already posted that one")
+        print("this was my tweet")
       }
     }
   }
 }
 
-#  post_tweet(status = paste("My first retweet from R for #untweetableSpecies ", species_tweet$status_url))  
-#  get_timeline('Untweeted Australian Species', n = 100, max_id = NULL, home = FALSE,
-#               parse = TRUE, check = TRUE, token = NULL, ...)
-  
-  
-#  for(t in species_tweet)
-#  {
-#    (tweetId  <- t$status_id)
-#    (tweetUrl <- t$status_url)
-#    (twwetText = t$text)
-#  }
+
   
